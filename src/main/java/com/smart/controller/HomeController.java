e88adf77cc3c03511701c1008d283f3ae73aefaa
Ayushi-garg-cs <ayushigarg2346@gmail.com>
2026-02-01 23:22:34 +0530

    Initial commit

---
+++ src/main/java/com/smart/controller/HomeController.java
@@ -0,0 +1,46 @@
+package com.smart.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.Contact;
+import com.smart.entities.User;
+
+@Controller
+public class HomeController {
+//	@Autowired
+//	private UserRepository userRepository;
+//	@GetMapping("/test")
+//	@ResponseBody
+//	public String test() {
+//		User user=new User();
+//		user.setName("Ayushi garg");
+//		user.setEmail("ayushigarg2346@gmail.com");
+//		Contact contact=new Contact();
+//		user.getContacts().add(contact);
+//		userRepository.save(user);
+//		return "Working";
+//	}
+	@RequestMapping("/")
+	public String home(Model m) {
+		m.addAttribute("title","Home-Smart Contact Manager");
+		return "home";
+	}
+	
+	@RequestMapping("/about")
+	public String about(Model m) {
+		m.addAttribute("title","About-Smart Contact Manager");
+		return "about";
+	}
+	@RequestMapping("/signin")
+	public String signin(Model m) {
+		m.addAttribute("title","Register-Smart Contact Manager");
+		return "signin";
+	}
+	
+}
