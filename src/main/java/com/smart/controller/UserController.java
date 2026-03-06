f792f47a309c82408cb7337090e369a8ad3dd7ea
Ayushi-garg-cs <ayushigarg2346@gmail.com>
2026-03-06 21:00:06 +0530

    updated on 21:00

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -5,10 +5,13 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.smart.dao.UserRepository;
+import com.smart.entities.Contact;
 import com.smart.entities.User;
 
 @Controller
@@ -18,15 +21,36 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
     
-    @RequestMapping("/index")
-    public String dashboard(Model model, Principal principal){
-
-        if(principal!=null){
+    @ModelAttribute
+    public void addCommonData(Model m,Principal principal) {
+    	if(principal!=null){
             String username = principal.getName();
             User user = userRepository.findByEmail(username);
-            model.addAttribute("user", user);
+            m.addAttribute("user", user);
         }
+    }
+    
+    @RequestMapping("/index")
+    public String dashboard(Model model){
+    	model.addAttribute("title","User Dashboard");  
         return "normal/user_dashboard";
+    }
+    
+    @GetMapping("/add-contact")
+    public String openAddContactForm(Model model) {
+    	model.addAttribute("title","Add Contact");
+    	model.addAttribute("contact",new Contact());
+    	return "normal/add_contact_form";
+    }
+    
+    @PostMapping("/process-contact")
+    public String processContact(@ModelAttribute Contact contact,Principal principal) {
+    	String name=principal.getName();
+    	User user=this.userRepository.findByEmail(name);
+    	contact.setUser(user);//bidirectional mapping
+    	user.getContacts().add(contact);
+    	this.userRepository.save(user);
+    	return "normal/add_contact_form";
     }
 }
 
