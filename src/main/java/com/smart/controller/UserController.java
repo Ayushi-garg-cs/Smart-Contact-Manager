9905b1d4ee2624eb618fd8f5912652b490457e1b
Ayushi-garg-cs <ayushigarg2346@gmail.com>
2026-03-07 21:14:40 +0530

    updated on 07-03-2026

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -18,10 +18,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
 import com.smart.entities.User;
+import com.smart.helper.Message;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/user")
@@ -53,26 +57,42 @@ public class UserController {
     }
     
     @PostMapping("/process-contact")
-    public String processContact(@ModelAttribute Contact contact,@RequestParam("profileImage") MultipartFile file, Principal principal) throws IOException  {
-    	String name=principal.getName();
-    	User user=this.userRepository.findByEmail(name);
+    public String processContact(@ModelAttribute Contact contact,@RequestParam("profileImage") MultipartFile file, Principal principal,RedirectAttributes redirectAttributes) throws IOException  {
     	
-    	if(file.isEmpty()) {
-    		//if file is empty then try our message
-    		System.out.println("File is empty");
-    	}else {
-    		//file the file to folder and update the name to contact
-    		contact.setImage(file.getOriginalFilename());
-    		File saveFile=new ClassPathResource("static/image").getFile();
-    		Path path=Paths.get(saveFile.getAbsolutePath() + File.separator + file.getOriginalFilename());
-    		Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
-    		
-    		System.out.println("Image is uploaded");
+    	try {
+	    	String name=principal.getName();
+	    	User user=this.userRepository.findByEmail(name);
+	    	
+	    	if(file.isEmpty()) {
+	    		//if file is empty then try our message
+	    		System.out.println("File is empty");
+	    	}else {
+	    		//file the file to folder and update the name to contact
+	    		contact.setImage(file.getOriginalFilename());
+	    		File saveFile=new ClassPathResource("static/image").getFile();
+	    		Path path=Paths.get(saveFile.getAbsolutePath() + File.separator + file.getOriginalFilename());
+	    		Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+	    		
+	    		System.out.println("Image is uploaded");
+	    	}
+	    	contact.setUser(user);//bidirectional mapping
+	    	user.getContacts().add(contact);
+	    	this.userRepository.save(user);
+	    	//message success
+	    	 redirectAttributes.addFlashAttribute(
+		        "message",
+		        new Message("Contact added successfully","success")
+		     );
+    	}catch(Exception e) {
+    		System.out.println("ERROR:"+e.getMessage());
+    		e.printStackTrace();
+    		//message error
+    		 redirectAttributes.addFlashAttribute(
+		        "message",
+		        new Message("Something went worng!!Try Again","danger")
+		     );
     	}
-    	contact.setUser(user);//bidirectional mapping
-    	user.getContacts().add(contact);
-    	this.userRepository.save(user);
-    	return "normal/add_contact_form";
+    	return "redirect:/normal/add_contact_form";
     }
 }
 
