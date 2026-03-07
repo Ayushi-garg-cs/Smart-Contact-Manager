3b3d5e086f7ffedb508da4bb5d39dc4c4d308199
Ayushi-garg-cs <ayushigarg2346@gmail.com>
2026-03-07 08:27:14 +0530

    updated on 07-03-2026

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -1,14 +1,23 @@
 package com.smart.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
@@ -44,9 +53,22 @@ public class UserController {
     }
     
     @PostMapping("/process-contact")
-    public String processContact(@ModelAttribute Contact contact,Principal principal) {
+    public String processContact(@ModelAttribute Contact contact,@RequestParam("profileImage") MultipartFile file, Principal principal) throws IOException  {
     	String name=principal.getName();
     	User user=this.userRepository.findByEmail(name);
+    	
+    	if(file.isEmpty()) {
+    		//if file is empty then try our message
+    		System.out.println("File is empty");
+    	}else {
+    		//file the file to folder and update the name to contact
+    		contact.setImage(file.getOriginalFilename());
+    		File saveFile=new ClassPathResource("static/image").getFile();
+    		Path path=Paths.get(saveFile.getAbsolutePath() + File.separator + file.getOriginalFilename());
+    		Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+    		
+    		System.out.println("Image is uploaded");
+    	}
     	contact.setUser(user);//bidirectional mapping
     	user.getContacts().add(contact);
     	this.userRepository.save(user);
