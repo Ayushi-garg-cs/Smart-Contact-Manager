d2ee9f34aa6f466a308fca976df06b80e7104cc7
Ayushi-garg-cs <ayushigarg2346@gmail.com>
2026-03-11 19:37:00 +0530

    updated on 11-03-2026

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -7,6 +7,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.security.Principal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
@@ -14,12 +15,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.smart.dao.ContactRepository;
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
 import com.smart.entities.User;
@@ -33,6 +36,9 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+    
+    @Autowired
+    private ContactRepository contactRepository;
     
     @ModelAttribute
     public void addCommonData(Model m,Principal principal) {
@@ -93,6 +99,19 @@ public class UserController {
 		     );
     	}
     	return "redirect:/user/add-contact";//redirect lgate time url return krte h naa ki html (view page) page//hello
+    }
+    
+    //handling show contacts
+    //per page=5(n)
+    //current page=0 (page)
+    @GetMapping("/show-contacts/{page}")
+    public String showContacts(@PathVariable("page") Integer page, Model m,Principal principal) {
+    	m.addAttribute("title","Show User Contacts");
+    	String userName=principal.getName();
+    	User user=this.userRepository.findByEmail(userName);
+    	List<Contact> contacts=this.contactRepository.findContactsByUser(user.getId());
+    	m.addAttribute("contacts",contacts);
+    	return "normal/show_contacts";
     }
 }
 
