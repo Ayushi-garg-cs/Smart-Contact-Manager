1396577c7e1e4e66d551d6240e50763893137c7a
Ayushi-garg-cs <ayushigarg2346@gmail.com>
2026-03-22 20:11:08 +0530

    updated on 22-03-2026

--- src/main/java/com/smart/dao/ContactRepository.java
+++ src/main/java/com/smart/dao/ContactRepository.java
@@ -9,8 +9,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.smart.entities.Contact;
+import com.smart.entities.User;
 
 public interface ContactRepository extends JpaRepository<Contact,Integer>{
 	@Query("from Contact as c where c.user.id=:userId")
 	public Page<Contact> findContactsByUser(@Param("userId") int userId, Pageable pageable);
+	
+	//search
+	public List<Contact> findByNameAndUser(String name,User user);
 }
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -10,3 +10,24 @@ const toggleSidebar=()=>{
 		$(".content").css("margin-left","20%");
 	}
 };
+
+const search=()=>{
+	let query=$("#search-input").val();
+	
+	if(query==""){
+		$("search-result").hide();
+	}else{
+		console.log(query);
+		//sending request to server
+		let url=`http://localhost:8082/search/${query}`;
+		
+		fetch(url)
+			.then((response)=>{
+				return response.json();
+			})
+			.then((data)=>{
+				console.log(data);
+			});
+		$(".search-result").show();
+	}
+}
